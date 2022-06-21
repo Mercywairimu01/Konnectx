0072f6822487a708539f1e37ec085a7692f4ef99
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-21 15:37:50 +0300

    creating artist profile

--- Konnectapp/models.py
+++ Konnectapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name_artist = models.CharField(max_length=255, blank=True,null=True)
+    Role_artist = models.CharField(max_length=255, blank=True, null=True)
+    location = models.CharField(max_length=25, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
+    social_links = models.ManyToManyField(social_links, blank=True)
