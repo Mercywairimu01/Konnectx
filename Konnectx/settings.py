86b27db364ee2f91608ca265ae37fb6498617887
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-21 21:37:13 +0300

    adding templates

--- Konnectx/settings.py
+++ Konnectx/settings.py
@@ -119,6 +119,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
