cf81cb4ea74342a22065813a6fb6959326f65079
Shalyne-Waweru <shalynewaweru@gmail.com>
2022-06-21 15:30:59 +0300

    Add and style the hamburger menu

--- Konnectapp/urls.py
+++ Konnectapp/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path,include
-
-
+from django import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-  
-    
+  path('', views.index, name="landingPage"),
 ]
