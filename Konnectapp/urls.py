e70e64269250b390f997174e42b0f54c111089f5
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-21 15:46:59 +0300

    add registration

--- Konnectapp/urls.py
+++ Konnectapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path,include
-
+from django.contrib.auth import views 
+from . import views
+from .views import *
 
 
 urlpatterns = [
-  
-    
+    path('',views.register,name ='register'),
+    path('artist_register/',views.artist_register.as_view(),name='artist_register'),
+    path('distributor_register/',views.distributor_register.as_view(),name='distributor_register'),
+ 
 ]
