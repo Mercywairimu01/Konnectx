ffa508bd1ecd5cdcb99752f9787c7ab17ba40c59
Mercywairimu01 <Mercywairimu01>
2022-06-22 10:27:59 +0300

    add migrations

--- Konnectapp/urls.py
+++ Konnectapp/urls.py
@@ -6,8 +6,8 @@ from .views import *
 
 urlpatterns = [
     path('', views.index, name="landingPage"),
-    path('',views.home,name='home'),
-    path('',views.register,name ='register'),
+    path('home/',views.home,name='home'),
+    path('register/',views.register,name ='register'),
     path('artist_register/',views.artist_register.as_view(),name='artist_register'),
     path('distributor_register/',views.distributor_register.as_view(),name='distributor_register'),
  
